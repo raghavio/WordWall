0d4bd68bb1b3f736bf75a2ceb7b3422b91a0118a
Raghav Sharma <raghav_ftw@hotmail.com>
2015-06-18 02:32:39 +0530

    Pronunciation done and some exception handling

--- WordWall.py
+++ WordWall.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 
 import requests
 
+def changeBackground(image_path):
+    lol = ""
+
 def get_center_x(x, text_size_x):
     center_x = (x - text_size_x) / 2
     return center_x
@@ -71,15 +74,26 @@ def draw_definition(draw, x, y, definition_data, p_data, synonyms_data):
 
 def get_dictionary_data(words):
     data = []
+    pronunciation_data = []
     for word in words:
         url = "http://www.oxforddictionaries.com/definition/english/" + word
-        page = requests.get(url, verify=False).text
+        while True:
+            try:
+                page = requests.get(url, verify=False).text
+                break
+            except (request.exceptions.ConnectionError):
+                print "Exception occured in get_dictionary_data()"
+                pass
         soup = BeautifulSoup(page, "lxml")
 
         results = soup.select('section.se1 > h3.partOfSpeechTitle >\
                                 span.partOfSpeech')[:3]
         part_of_speech = [result.text for result in results]
-
+        pronun_result_raw = soup.find("div", class_="headpron")
+        pronunciation = "" if pronun_result_raw is None else\
+                        pronun_result_raw.find(text=True, recursive=False)\
+                            .replace('/','[', 1).replace('/', ']', 1).strip()
+        pronunciation_data.append(pronunciation)
         count = len(part_of_speech)
         definition_data = []
         for i in range(0, count):
@@ -95,24 +109,26 @@ def get_dictionary_data(words):
                 example = None if not results_2_raw else '"'+results_2_raw[0].text+'"'
                 num_bullet = str(j+1)+".  " if limit == 2 else "1.  "
 
-                dictionary = {'definition' : num_bullet + definition.text.strip(), 'example' : example, 'num_bullet' : num_bullet}
+                dictionary = {'definition' : num_bullet + definition.text.strip(),
+                                'example' : example, 'num_bullet' : num_bullet}
                 definition_data.append([part_of_speech[i], dictionary])
         data.append(definition_data)
-    return data
+    return data, pronunciation_data
 
 def draw_pronunciation(draw, x, y, pronunciations, words_data):
     word_1_y, word_2_y, word_size_y = words_data
 
-    font = ImageFont.truetype("Pronunciation.ttf", 24)
+    font = ImageFont.truetype("Pronunciation.otf", 22)
 
     pronun_1_size = draw.textsize(pronunciations[0], font)
     pronun_2_size = draw.textsize(pronunciations[1], font)
 
+    space = 5
     pronun_1_x = get_center_x(x, pronun_1_size[0])
-    pronun_1_y = (word_1_y - pronun_1_size[1])
+    pronun_1_y = (word_1_y - pronun_1_size[1] - space)
 
     pronun_2_x = get_center_x(x, pronun_2_size[0])
-    pronun_2_y = (word_2_y + word_size_y)
+    pronun_2_y = (word_2_y + word_size_y + space)
 
     draw.text((pronun_1_x, pronun_1_y), pronunciations[0], font=font)
     draw.text((pronun_2_x, pronun_2_y), pronunciations[1], font=font)
@@ -120,9 +136,6 @@ def draw_pronunciation(draw, x, y, pronunciations, words_data):
     pronunciation_data = {'pronun_1_y' : pronun_1_y, 'pronun_2_y' : pronun_2_y,
                             'pronun_2_textsize_y' : pronun_2_size[1]}
     return pronunciation_data
-
-def get_pronunciation(words):
-    return ["[kwid-nuhngk]", "[Pro-nun-ci-at-ion]"]
 
 def draw_words(draw, x, y, words):
     font = ImageFont.truetype("Words.ttf", 56)
@@ -174,14 +187,20 @@ def draw_synonyms(draw, x, y, synonyms_1, synonyms_2): #To be scaled
 
 def get_synonyms(word):
     url = "http://www.thesaurus.com/browse/" + word
-    page = requests.get(url, verify=False).text
+    while True:
+        try:
+            page = requests.get(url, verify=False).text
+            break
+        except (request.exceptions.ConnectionError):
+            print "Exception occured in get_synonyms()"
+            pass
     soup = BeautifulSoup(page, "lxml")
     synonyms = []
 
     result = soup.select('div#synonyms-0 > div.filters > \
                                     div.relevancy-block > div.relevancy-list > \
                                     ul > li > a > span.text')[:10]
-    synonyms.extend([str(synonym.text.title()) for synonym in result])
+    synonyms.extend([synonym.text.title().encode('utf-8') for synonym in result])
     return synonyms
 
 def draw_quote(draw, x, y, quote, author):
@@ -192,27 +211,36 @@ def draw_quote(draw, x, y, quote, author):
         font_size -= 1
         font = ImageFont.truetype("Quote.ttf", font_size)
         quote_size = draw.textsize(quote, font)
+
     author_size = draw.textsize(author, font)
-    center_x = get_center_x(x, quote_size[0])
-    center_y = (y - (quote_size[1] + author_size[1])) / 2
-    draw.text((center_x, center_y), quote, font=font)
-    draw.text((get_center_x(x, author_size[0]), center_y + quote_size[1]), author, font=font)
+    center_x_quote = get_center_x(x, quote_size[0])
+    center_y_quote = (y - (quote_size[1] + author_size[1])) / 2
+    draw.text((center_x_quote, center_y_quote), quote, font=font)
+
+    center_x_author = get_center_x(x, author_size[0])
+    center_y_author = center_y_quote + quote_size[1]
+    draw.text((center_x_author, center_y_author), author, font=font)
 
 def get_quote():
     api_url = "http://api.theysaidso.com/qod"
     default_quote = '"Don\'t cry because it\'s over, smile because it happened."'
     default_quote_author = 'Dr. Seuss'
-
-    json = requests.get(api_url).json()
+    while True:
+        try:
+            json = requests.get(api_url).json()
+            break
+        except (requests.exceptions.ConnectionError):
+            print "Exception occured in get_quote()"
+            pass
     content = json.get('contents')
 
     if content is not None:
         quote = content['quote']
         author = content['author']
 
-        return (quote, author)
+        return quote, author
     else:
-        return (default_quote, default_quote_author)
+        return default_quote, default_quote_author
 
 def main():
     img = Image.open("image.jpg")#.crop((0,0,1440,900))
@@ -231,15 +259,15 @@ def main():
 
     words_data = draw_words(draw, x, y, words)
 
-    pronunciations = get_pronunciation(words)
-    if pronunciations is not None:
-        p_data = draw_pronunciation(draw, x, y, pronunciations, words_data)
+    data, pronunciations = get_dictionary_data(words)
 
-    data = get_dictionary_data(words)
+    p_data = draw_pronunciation(draw, x, y, pronunciations, words_data)
     draw_definition(draw, x, y, data, p_data, synonyms_data)
 
 
     img.save('sample-out.jpg')
+    image_path = None
+    changeBackground(image_path)
 
 if __name__ == "__main__":
     main()
